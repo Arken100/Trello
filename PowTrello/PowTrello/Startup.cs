65ef5393e19ba1de7162e85ced6e3583e13b5980
lukaszloba <lukasz.loba@gmail.com>
2016-04-21 21:30:04 +0200

    Initial commit

---
+++ PowTrello/PowTrello/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PowTrello.Startup))]
+namespace PowTrello
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
