cde0fea97a6317797dc67e72bcc38fd70dcc56fe
lukaszloba <lukasz.loba@gmail.com>
2016-04-21 23:22:58 +0200

    DAL + Board (class for test)

---
+++ PowTrello/PowTrello/DAL/DTContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PowTrello.Models;
+
+namespace PowTrello.DAL
+{
+    public class DTContext : DbContext
+    {
+        public DTContext() : base("TrelloConnectionString")
+        {
+            System.Data.Entity.Database.SetInitializer(new MigrateDatabaseToLatestVersion<DTContext, ConfigurationDb>());
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+
+        }
+
+        public DbSet<Board> Board { get; set; }
+        // public DbSet<CurrencyInformation> CurrencyInformations { get; set; }
+
+        
+    }
+}
---
+++ PowTrello/PowTrello/Models/Board.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace PowTrello.Models
+{
+    public class Board : Base
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+
+
+}
