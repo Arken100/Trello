6ba5e86799d0f66d11e1dfe7e1a16ba4e51c0cd6
lukaszloba <lukasz.loba@gmail.com>
2016-05-08 23:23:02 +0200

    Dodane role Admin i LeloUser + Seed domyślnych kont

---
+++ Lelo/Lelo/Controllers/BaseController.cs
@@ -0,0 +1,38 @@
+﻿using Lelo.DAL;
+using Lelo.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lelo.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected ApplicationDbContext db = new ApplicationDbContext();
+        protected Guid CurrentUserId;
+        protected ApplicationUser CurrentUser;
+
+
+        public void BoardsController()
+        {
+            CurrentUserId = User != null ? new Guid(User.Identity.GetUserId()) : Guid.Empty;
+            CurrentUser = CurrentUserId != Guid.Empty ? db.Users.First(x => x.Id == CurrentUserId) : null;
+        }
+
+        public Guid GetCurrentUserId()
+        {
+            var toReturn = User != null ? new Guid(User.Identity.GetUserId() == null ? Guid.Empty.ToString() : User.Identity.GetUserId().ToString()) : Guid.Empty;
+            return toReturn;
+        }
+
+        public void SetCurrentUser()
+        {
+            CurrentUserId = User != null ? new Guid(User.Identity.GetUserId() == null ? Guid.Empty.ToString() : User.Identity.GetUserId().ToString()) : Guid.Empty;
+            CurrentUser = CurrentUserId == Guid.Empty ? null : db.Users.First(x => x.Id == CurrentUserId);
+        }
+
+    }
+}
--- Lelo/Lelo/Controllers/HomeController.cs
+++ Lelo/Lelo/Controllers/HomeController.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Lelo.DAL;
+using Lelo.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,12 +10,66 @@ using System.Web.Mvc;
 
 namespace Lelo.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
+
         public ActionResult Index()
         {
             return View();
         }
+
+        public ActionResult Init()
+        {
+
+
+
+            //List<Role> Roles = new List<Role>
+            //{
+            //    new Role{
+            //    Name = "Admin"
+            //    },
+            //    new Role
+            //    {
+            //        Name = "LeloUser"
+            //    }
+            //};
+
+            //Roles.ForEach(delegate (Role role)
+            //    {
+            //        db.Roles.Add(role);
+            //    });
+
+            ApplicationDbContext dbc = new ApplicationDbContext();
+            string[] roles = { "Admin", "LeloUser" };
+
+            foreach (string role in roles)
+            {
+                ApplicationUser userToCreate = new ApplicationUser();
+               // var UserManager = new UserManager<ApplicationUser, Guid>(new UserStore(db));
+
+                var UserManager = new UserManager<ApplicationUser, Guid>(new UserStore<ApplicationUser, Role, Guid, UserLogin, UserRole, UserClaim>(db));
+                var RoleManager = new RoleManager<Role, Guid>(new RoleStore(db));
+
+                userToCreate.UserName = userToCreate.Email = role.ToLower() + "@example.com";
+
+                var adminresult = UserManager.Create(userToCreate, "123456");
+
+                if (!RoleManager.RoleExists(role))
+                {
+                    var roleresult = RoleManager.Create<Role, Guid>(new Role(role));
+                }
+
+                if (adminresult.Succeeded)
+                {
+                    var result = UserManager.AddToRole(userToCreate.Id, role);
+                }
+            }
+
+
+
+            return Content("222");
+        }
+
 
         public ActionResult About()
         {
@@ -26,5 +84,8 @@ namespace Lelo.Controllers
 
             return View();
         }
+
+
+
     }
 }
