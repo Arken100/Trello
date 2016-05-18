69bf798317416849077d7aeffb0110f9bd0026ca
lukaszloba <lukasz.loba@gmail.com>
2016-05-18 22:17:44 +0200

    sort kolumn bez ajaxa (chwilowo)

--- Lelo/Lelo/App_Start/BundleConfig.cs
+++ Lelo/Lelo/App_Start/BundleConfig.cs
@@ -8,14 +8,19 @@ namespace Lelo
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            BundleTable.EnableOptimizations = false;
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-            "~/Scripts/jquery-ui-{version}.js"));
+                //this (DRAGTABLE) must be before jqueryui. it uses widget
+                "~/Scripts/dragtable-2.0.12/jquery.dragtable.js",
+                "~/Scripts/jquery-ui-{version}.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
@@ -31,6 +36,7 @@ namespace Lelo
                       "~/Content/site.css"));
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
+                "~/Scripts/dragtable-2.0.12/dragtable.css",
               "~/Content/themes/base/jquery.ui.core.css",
               "~/Content/themes/base/jquery.ui.resizable.css",
               "~/Content/themes/base/jquery.ui.selectable.css",
